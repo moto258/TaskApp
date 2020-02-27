e38f6089588e69b4dda9d1af0e81ed8930a10a0e
moto258 <ymmt369@gmail.com>
2020-02-27 09:05:36 +0900

    Initial Commit

---
+++ TaskApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskApp
+//
+//  Created by asamuzak on 2020/02/27.
+//  Copyright © 2020 asamuzak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
