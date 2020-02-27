7dbc1a47eef8fbca44d523e0fb45cc1ead0f2d43
moto258 <ymmt369@gmail.com>
2020-02-27 22:20:02 +0900

    pod install and gitignore add

---
+++ TaskApp/inputViewController.swift
@@ -0,0 +1,34 @@
+//
+//  inputViewController.swift
+//  TaskApp
+//
+//  Created by asamuzak on 2020/02/27.
+//  Copyright © 2020 asamuzak. All rights reserved.
+//
+
+import UIKit
+
+class inputViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var dataPicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
